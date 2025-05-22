d90c50f6a35c7edb2387f5121e367201667add02
FURQAAN194 <furqansaira91@gmail.com>
2025-05-22 17:47:03 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,15 @@
+
+import math
+
+# Get input from user
+num = float(input("Enter a number: "))
+
+# Calculations
+sqrt_val = math.sqrt(num)
+log_val = math.log(num)
+sine_val = math.sin(num)
+
+# Output
+print(f"Square root of {num} is: {sqrt_val}")
+print(f"Natural log of {num} is: {log_val}")
+print(f"Sine of {num} radians is: {sine_val}")
